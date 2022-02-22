e5f46f241e3b5b86e7dd5c3ae6bc09b339d90415
Vlad Nemirovskii <v.nemirovskii@viseven.com>
2022-02-22 13:28:42 +0200

    added component - title-shoplist

--- test-angular/src/app/app.component.ts
+++ test-angular/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'title-shoplist',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ test-angular/src/app/items-list-component/items-list-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-list-component',
+  templateUrl: './items-list-component.component.html',
+  styleUrls: ['./items-list-component.component.css']
+})
+export class ItemsListComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
