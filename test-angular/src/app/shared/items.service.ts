d6d7935d4d77f024ebde6f180aab482a3266947b
Vlad Nemirovskii <v.nemirovskii@viseven.com>
2022-02-23 13:35:06 +0200

    added some styles

--- test-angular/src/app/shared/items.service.ts
+++ test-angular/src/app/shared/items.service.ts
@@ -7,30 +7,30 @@ export interface item {
     id: number
 }
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ItemsService {
 
     public items: item[] = [
-        { id:0, name: 'Сыр', checked: false},
-        { id:1, name: 'Колбаса', checked: false},
-        { id:2,  name: 'Молоко', checked: false},
-        { id:3,  name: 'Хлеб', checked: false},
-        { id:4, name: 'Пивко', checked: false},
+        { id: 0,name: 'Сыр', checked: false },
+        { id: 1, name: 'Колбаса', checked: false },
+        { id: 2, name: 'Молоко', checked: false },
+        { id: 3, name: 'Хлеб', checked: false },
+        { id: 4, name: 'Пивко', checked: false },
     ]
 
-    onToggle(id: number ) {
-        let idx = this.items.findIndex( t => t.id === id)
-       
-        this.items[idx].checked = !this.items[idx].checked 
-  
+    onToggle(id: number) {
+        let idx = this.items.findIndex(t => t.id === id)
+
+        this.items[idx].checked = !this.items[idx].checked
+
         if (this.items[idx].checked) {
             this.items.push(this.items[idx])
-            this.items.splice(this.items.findIndex( t => t.id === id) , 1)
-        } else {         
-         this.items.splice(0, 0, this.items.splice(idx, 1)[0]);
+            this.items.splice(this.items.findIndex(t => t.id === id), 1)
+        } else {
+            this.items.splice(0, 0, this.items.splice(idx, 1)[0]);
         }
-        
-      }
+
+    }
 
 
 }
